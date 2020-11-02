877f6054260eaae2318f850ff745cfa282f567a2
tbriany <brianytaveras@pursuit.org>
2020-11-01 21:18:08 -0500

    creates variable for current index in tenQuestions array, and creates a variable for currentQuestion object

--- trivia_game/src/Components/Game.jsx
+++ trivia_game/src/Components/Game.jsx
@@ -6,11 +6,17 @@ import Question from './Question'
 function Game() {
 
     const [tenQuestions, setTenQuestions] = useState(questions.sort(() => 0.5 - Math.random()).slice(0, 10))
+    const [currQindex, setCurrQindex] = useState(0)
+    const [currQ, setCurrQ] = useState(tenQuestions[currQindex])
 
     return (
         <div className="Game">
             <h1>Game Started!</h1>
-            <Question/>
+            <Question
+                question={currQ.question}
+                incorrect={currQ.incorrect}
+                correct={currQ.correct}
+            />
         </div>
     );
 }
