4c73a8319cc70457e5439a47dc83bafe0cfee27c
tbriany <brianytaveras@pursuit.org>
2020-11-01 21:07:45 -0500

    imports random 10 questions from json file

--- trivia_game/src/Components/Game.jsx
+++ trivia_game/src/Components/Game.jsx
@@ -1,10 +1,17 @@
+import { useState, useEffect } from "react";
+import '../App.css'
+import questions from '../json/Apprentice_TandemFor400_Data.json'
+
 
 function Game() {
-  return (
-    <div className="Game">
-      <h1>Game Started!</h1>
-    </div>
-  );
+
+    const [tenQuestions, setTenQuestions] = useState(questions.sort(() => 0.5 - Math.random()).slice(0, 10))
+
+    return (
+        <div className="Game">
+            <h1>Game Started!</h1>
+        </div>
+    );
 }
 
 export default Game;
