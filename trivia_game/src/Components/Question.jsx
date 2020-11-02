d34c0d4d0ed8a7b9a09951dbf89c9f898aa8fbc6
tbriany <brianytaveras@pursuit.org>
2020-11-01 21:18:58 -0500

    creates answers array where answers and rearranged in random order

--- trivia_game/src/Components/Question.jsx
+++ trivia_game/src/Components/Question.jsx
@@ -4,10 +4,30 @@ import '../App.css'
 
 function Question(props) {
 
+    const { question, correct, incorrect } = props
+    const [answers, setAnswers] = useState([])
+
+    useEffect(() => {
+        setAnswers([...incorrect, correct].sort(() => 0.5 - Math.random()))
+    }, [incorrect, correct])
+
+    const answerArr = answers.map((answer) => {
+        return (
+            <div key={answer}>
+                <label>
+                    <input type="radio" name="chosenAnswer" value={answer} />
+                    {answer}
+                </label>
+            </div>
+        )
+    })
 
     return (
         <div className="Question">
-            <h2>Question Goes Here</h2>
+            <h3>{question}</h3>
+            <form >
+                {answerArr}
+            </form>
         </div>
     );
 }
