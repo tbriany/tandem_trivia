f88397cfb4da7019b037c1f253d474468d2b587c
tbriany <brianytaveras@pursuit.org>
2020-11-01 20:53:10 -0500

    creates start game button

--- trivia_game/src/App.js
+++ trivia_game/src/App.js
@@ -1,9 +1,21 @@
+import {useState} from "react";
 import './App.css';
+import Game from './Components/Game'
 
 function App() {
+
+  const [gameStarted, setgameStarted] = useState(false)
+
   return (
     <div className="App">
-      <h1>Welcome to Trivia!</h1>
+      {gameStarted
+        ? <Game />
+        : 
+        <div>
+        <h1>Welcome to Trivia!!</h1>
+        <button onClick={prevState => setgameStarted(true)}>Start Game</button> 
+        </div>
+      }
     </div>
   );
 }
